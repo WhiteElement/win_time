e71f753146bb24f8ab8bee23473eaf38b2893578
Manuel Braunwald <manuelbrusche@gmail.com>
2025-05-20 20:36:18 +0200

    refactor

--- src/main.zig
+++ src/main.zig
@@ -1,41 +1,59 @@
 const std = @import("std");
 
+const CmdArgs = struct {
+    iter: std.process.ArgIterator,
+    collectedArgs: std.ArrayList([:0]const u8),
+
+    pub fn init(allocator: std.mem.Allocator) !CmdArgs {
+        return CmdArgs{
+            .iter = try std.process.argsWithAllocator(allocator),
+            .collectedArgs = std.ArrayList([:0]const u8).init(allocator),
+        };
+    }
+
+    pub fn getArgs(self: *CmdArgs) ![]const []const u8 {
+        try self.collectedArgs.append("cmd.exe");
+        try self.collectedArgs.append("/C");
+
+        var skippedFirst = false;
+        while (true) {
+            const arg = self.iter.next();
+            if (!skippedFirst) {
+                skippedFirst = true;
+                continue;
+            }
+
+            if (arg) |a| {
+                try self.collectedArgs.append(a);
+            } else {
+                break;
+            }
+        }
+
+        const slice: []const []const u8 = self.collectedArgs.items;
+        return slice;
+    }
+
+    pub fn deinit(self: *CmdArgs) void {
+        self.iter.deinit();
+        self.collectedArgs.deinit();
+    }
+};
+
 pub fn main() !void {
     var buffer: [4 * 1024]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&buffer);
     const allocator = fba.allocator();
-    var iter = try std.process.argsWithAllocator(allocator);
-    defer iter.deinit();
     const stdout = std.io.getStdOut();
 
-    var allArgs = std.ArrayList([:0]const u8).init(allocator);
-    try allArgs.append("cmd.exe");
-    try allArgs.append("/C");
-
-    var idx: u8 = 0;
-    outer: while (true) {
-        const arg = iter.next();
-        if (idx == 0) {
-            idx += 1;
-            continue;
-        }
-
-        if (arg) |a| {
-            try allArgs.append(a);
-        } else {
-            break :outer;
-        }
-    }
+    var args = try CmdArgs.init(allocator);
+    const commands = try args.getArgs();
 
     var timer = try std.time.Timer.start();
-    std.debug.print("len: {d}\n", .{allArgs.items.len});
-    for (allArgs.items) |a| std.debug.print("Arg: {s}\n", .{a});
-    const slice: []const []const u8 = allArgs.items;
-    var proc = std.process.Child.init(slice, allocator);
+    var proc = std.process.Child.init(commands, allocator);
     _ = try proc.spawnAndWait();
 
     _ = try stdout.write(try formatTS(allocator, timer.read()));
-    std.debug.print("EndIndex of fba '{d}', length '{d}'", .{ fba.end_index, buffer.len });
 }
 
 fn formatTS(allocator: std.mem.Allocator, ns: u64) ![]const u8 {
